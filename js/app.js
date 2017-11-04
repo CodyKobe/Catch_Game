2b553a4fe312d8a267fc47045eba4882d37333df
CodyKobe <pawel.tutak@outlook.com>
2017-11-04 23:17:12 +0100

    Point 4

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('hello');
+console.log( 'hello' );
+
+function Furry( x, y ) {
+    this.x = 0;
+    this.y = 0;
+    this.direction = "right";
+
+}
+
+function Coin() {
+    this.x = Math.floor( Math.random() * 10 );
+    this.y = Math.floor( Math.random() * 10 );
+
+}
